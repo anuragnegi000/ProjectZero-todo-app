013f2578e8b70e3f93d3aa2479702e86daf34ba7
anuragnegi000 <anurag.negi862@gmail.com>
2024-08-11 21:21:08 +0530

    latest backend initialised

--- backend/router/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router=express.Router();
-const {auth}=require("../middleware");
-const userRouter=require("./user.JS");
-
-router.use("/api/v1",userRouter);
-export default router;
